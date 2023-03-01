04784e5138da0026ca50bedaee278f97f3d655f2
vinod <vinodpr737947@gmail.com>
2023-03-01 10:54:44 +0530

    day one completed

---
+++ Day 1/index.js
@@ -0,0 +1,20 @@
+/*  this is how we create element in plain javascript*/
+
+// const child=document.createElement('h1');
+// child.innerText="namste React bro";
+// const root=document.getElementById("root");
+// root.appendChild(child);
+
+
+                              // type ||    props     ...children can be aarray  
+                              const heading1=React.createElement('h1',   {id:"heading"},      "Jay Mata di");
+                              const heading2=React.createElement('h2',   {id:"heading"},      "Jay Shree ram");
+                      
+                      
+                              //we can render more than 2 element because children can be given in the form of array
+                              
+                              const parent=React.createElement('div',{className:"parent"},[heading1,heading2]);
+                      
+                              const root=ReactDOM.createRoot(document.getElementById("root"));
+                              root.render(parent);
+                             
